ef231d6ba6ffd03d34e3855642558aa13290f586
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2021-12-10 01:03:35 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,51 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-trainEnregistrement = []
-listTrains ={}
-voiePassage = True
+trainEnregistrement = []#enregistrement des trains
+trainEnregistrementPipe = []#enregistrement des voies de communications avec les différents trains
+voieGarePrincipale = [] #la ou les voies qui servent à sortir de gare
+voieGarage = [] #les différentes voies interne de la gare
+garage = 4 #4 places de garage
+trainAttenteRentrer = []
+trainAttenteSortir = []
+
+
+
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
+
+
+#processus des trains
+def trainFils(pipeWrite,pipeRead):
+    while(1):
+        action = pipeRead.recv()
+        if (action != "rien"):
+            if action == "entrer" :
+                pipeWrite.send()
+
+            elif action == "sortir" :
+                pipeWrite.send()
+            action = "rien"
+
+def voie_libre() :
+    return len(voieGarePrincipale) == 0
+
+def reste_place_dans_garage() :
+    return len(voieGarage) < garage
+
+def ajoute_train_garage(numero_train):
+    if (reste_place_dans_garage()):
+        voieGarage.append(numero_train)
+        return 1
+    else :
+        return -1
+def ajoute_train_voie_principale(numero_train):
+    if (voie_libre()):
+        voieGarePrincipale.append(numero_train)
+        return 1
+    else :
+        return -1
 
 # Create server
 with SimpleXMLRPCServer(('localhost', 8000),
@@ -34,11 +73,16 @@ with SimpleXMLRPCServer(('localhost', 8000),
             return False
         else:
             trainEnregistrement.append(num)
+            pipeRead, pipeWrite = Pipe()
+            trainEnregistrementPipe.append([pipeRead, pipeWrite])
+            p = Process( target=trainFils, args=( [pipeWrite,pipeRead] ) )
+            p.start()
             print("train ajouté avec succès")
             return True
     server.register_function(enregistrer_train, 'enregistrer')
 
     def entrer_en_gare(x):
+
         print("le train " + x + "souhaite rentrer en gare" )
         return 1
     server.register_function(entrer_en_gare, 'entrer')
