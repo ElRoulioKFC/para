177e0d0c0f3e62f527c2bbd8aec4b1e6908860d8
Julien Valverdé <julien.valverde@mailo.com>
2021-12-13 03:29:05 +0100

    Quasiment fonctionnel (et pas grâce à Python)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from train import *
+from time import sleep
 
 
 waiting_tracks = Platform("WAITING_TRACKS")  # Voies d'attente en entrée de la plateforme
@@ -32,3 +33,37 @@ train_arrival()
 # On positionne un train en stationnement
 track = sidings.free_track()
 track.train = Train(track, DIRECTION_EXIT)
+
+
+# Main loop
+while True:
+    for train in trains:
+        # Traitement des messages envoyés par les trains
+        if train.parent_conn.poll():
+            train.process_data(train.parent_conn.recv())
+
+        # Si un train est attente, on regarde si toute les conditions sont réunies pour le faire partir
+        if train.waiting:
+            main_track = main_tracks.free_track()
+
+            if   train.track.platform.name == "WAITING_TRACKS":
+                destination_track = sidings.free_track()
+
+            elif train.track.platform.name == "SIDINGS":
+                destination_track = waiting_tracks.free_track()
+                if (destination_track == None):
+                    destination_track = waiting_tracks.add_track()
+
+            if main_track and destination_track:  # Si la voie est libre, on envoie le train !
+                print(str(train.id) + " a reçu une autorisation d'avancer.")
+
+                train.waiting = False
+                # \/ buggué pour le moment, merci Python de merde
+                train.parent_conn.send({
+                    "instructions": [main_track, destination_track]
+                })
+
+                sleep(0.5)  # Permet d'éviter certains conflits
+
+
+    sleep(0.01)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from math import floor
 from random import uniform
 from multiprocessing import *
+from time import sleep
 
 
 class Track:
@@ -14,6 +15,9 @@ class Track:
     def has_train(self):
         self.train != None
 
+    def remove_train(self):
+        self.train = None
+
 
 class Platform():
     """Un ensemble de voies"""
@@ -23,7 +27,7 @@ class Platform():
         self.tracks = []
 
     def add_track(self, travel_time = 0):
-        track = Track(self.name, travel_time)
+        track = Track(self, travel_time)
         self.tracks.append(track)
 
         return track
@@ -35,6 +39,7 @@ class Platform():
         self.tracks.remove(track)
 
     def free_track(self):
+        """Retourne la première voie libre de la plateforme"""
         for track in self.tracks:
             if not track.has_train():
                 return track
@@ -45,12 +50,14 @@ class Platform():
 DIRECTION_ENTRY = 1  # Entrée dans la plateforme
 DIRECTION_EXIT  = 2  # Sortie de la plateforme
 
+trains = []
+
 class Train:
     """Un train"""
 
-    def __init__(self, initial_position, initial_direction):
+    def __init__(self, initial_track, initial_direction):
         self.id = floor(uniform(1000, 9000))
-        self.track = initial_position         # Voie sur laquelle le train se situe
+        self.track = initial_track            # Voie sur laquelle le train se situe
         self.direction = initial_direction    # Direction vers le train se dirige (entrée ou sortie de la plateforme)
 
         self.parent_conn, self.child_conn = Pipe()
@@ -58,7 +65,91 @@ class Train:
         self.process = Process(target=train_process, args=(self.id, self.track, self.direction, self.child_conn,))
         self.process.start()
 
+        trains.append(self)
 
-def train_process(id, track, direction, conn):
+    def destroy(self):
+        self.track.train = None
+        trains.remove(self)
+
+    def process_data(self, data):
+        """Traite les données reçues par le processus du train"""
+
+        # Le train a effectué une demande
+        if ("demand" in data) and (data["demand"] == "MOVE"):
+            if   self.track.platform.name == "WAITING_TRACKS":
+                print(str(self.id) + " requiert un accès à la plateforme.")
+            elif self.track.platform.name == "SIDINGS":
+                print(str(self.id) + " requiert une sortie de la plateforme.")
+
+            self.waiting = True
+
+        # Le train s'est déplacé sur une nouvelle voie
+        if "moved" in data:
+            moved = data["moved"]
+
+            if moved["track"].has_train():  # <== Pas normal !
+                raise Exception("Track already has a train on it (you just caused a deadly accident :( )!")
+
+            print(str(self.id) + " se déplace sur " + self.track.platform.name)
+
+            self.track.remove_train()
+
+            self.track = moved["track"]
+            self.direction = moved["direction"]
+
+            self.track.train = self
+
+        # Le train est arrivé à sa destination
+        if ("arrived" in data) and data["arrived"]:
+            print(str(self.id) + " est arrivé à " + self.track.platform.name)
+
+            if self.track.platform.name == "WAITING_TRACKS":  # Le train est parti, arrêter son processus
+                self.destroy()
+
+
+def train_process(id, initial_track, initial_direction, conn):
     """Processus fils de chaque train"""
-    pass
+
+    active = True
+    track = initial_track
+    direction = initial_direction
+
+
+    def train_process_move():
+        """Instructions de déplacement d'un processus fils de train"""
+
+        # Indiquer à l'opérateur que l'on souhaite bouger
+        conn.send({"demand": "MOVE"})
+
+        # Attendre la réponse de l'opérateur
+        instructions = conn.recv()["instructions"]
+
+        # Choix de la direction du train
+        if   track.platform.name == "WAITING_TRACKS":
+            direction = DIRECTION_ENTRY
+        elif track.platform.name == "SIDINGS":
+            direction = DIRECTION_EXIT
+
+        conn.send({"moved": {"track": track, "direction": direction}})
+
+        # Exécution des instructions données par l'opérateur
+        for to_track in instructions:
+            sleep(track.travel_time)
+
+            track = to_track
+            conn.send({"moved": {"track": track, "direction": direction}})
+
+        conn.send({"arrived": True})
+
+        # Si arrivé en sortie de la plateforme, supprimer le train du système
+        if track.platform.name == "WAITING_TRACKS":
+            active = False
+
+
+    while active:
+        if track.platform.name == "SIDINGS":
+            # On simule le temps de stationnement à quai (aléatoirement de 10 à 90s)
+            sleep(floor(uniform(5, 30)))
+
+        train_process_move()
+        sleep(0.01)
